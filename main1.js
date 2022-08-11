8710b748351521bff687d10857b5055253daea5c
marcosraccoon <marcos123.mdnr@gmail.com>
2022-08-11 15:34:14 -0500

    Profe.. Olvidé hacer el commit en clase

---
+++ main1.js
@@ -0,0 +1,49 @@
+let textIn;
+let textBtnAdd;
+let textList;
+let textBtnDel;
+let listCount = 0;
+let isDeletingActive = false;
+
+function toggleDel() {
+	if (isDeletingActive) {
+		isDeletingActive = false;
+		textBtnDel.innerHTML = "Borrar";
+	} else if (!isDeletingActive) {
+		isDeletingActive = true;
+		textBtnDel.innerHTML = "PULSA PARA BORRAR";
+	}
+}
+
+function toLowerC() {
+	if (textIn.value.length > 0) {
+		let txtLW = textIn.value.toLowerCase();
+		createItemList(txtLW);
+		textIn.value = "";
+	}
+}
+
+function createItemList(txt) {
+	// textList.innerHTML += "<li id='itemLs"+listCount+"'>"+txt
+	let content = document.createTextNode(txt);
+	let newItemList = document.createElement("li");
+	newItemList.setAttribute("id", "itemLs"+listCount);
+	newItemList.appendChild(content);
+	textList.appendChild(newItemList);
+	newItemList.onclick = function () {
+		if (isDeletingActive) {
+			this.parentNode.removeChild(this);
+		}
+	}
+	listCount++;
+}
+
+function init() {
+	textIn = document.getElementById('textIn');
+	textBtnAdd = document.getElementById('textBtnAdd');
+	textList = document.getElementById('textList');
+	textBtnDel = document.getElementById('textBtnDel');
+
+	textBtnAdd.onclick = toLowerC;
+	textBtnDel.onclick = toggleDel;
+}
